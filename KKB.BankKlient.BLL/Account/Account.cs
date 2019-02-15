f7478187eb6b3959971a66957e5e5c93a5b862f9
КахаровЖ <@206_11>
2019-02-15 12:15:46 +0600

    Add project files.

---
+++ KKB.BankKlient.BLL/Account/Account.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KKB.BankKlient.BLL.Account
+{
+    public enum currency { usd,kzt,rur}
+    public class Account
+    {
+        public int id { get; set; }
+        public int UserId { get; set; }
+        public string Number { get; set; }
+        public DateTime CreataDate { get; set; }
+        public currency Currency { get; set; }
+        public  decimal Balance { get; set; }
+    }
+}
---
+++ KKB.BankKlient.BLL/Account/ServiceAccount.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KKB.BankKlient.BLL.User;
+using LiteDB;
+
+namespace KKB.BankKlient.BLL.Account
+{
+    public class ServiceAccount
+    {
+        Random rnd = new Random();
+        public Account GetAccount(User.User user, currency cur)
+        {
+            Account account = new Account();
+            account.CreataDate = DateTime.Now.AddMonths(rnd.Next(1, 12) * -1);
+            account.Number = cur.ToString().ToUpper() + rnd.Next();
+            account.Balance = 0;
+            account.UserId = user.Id;
+            account.Currency = cur;
+            return account;
+        }
+        public bool CreateAccountDb(Account account, out string message)
+        {
+            try
+            {
+                using (var db = new LiteDatabase(@"kkb.db"))
+                {
+                    var accounts = db.GetCollection<Account>("Account");
+
+                    accounts.Insert(account);
+                }
+                message = "Создание счёта прошла успешно";
+                return true;
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+                return false;
+            }
+        }
+    };
+}
